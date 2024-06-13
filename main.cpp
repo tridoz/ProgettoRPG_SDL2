c9f14b15bf41ffec3af048655c909eeafb1802fa
tridoz <149873185+tridoz@users.noreply.github.com>
2024-06-13 19:17:23 +0200

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int numero;
-    std::cout << "Inserisci un numero intero: ";
-    std::cin >> numero;
-    std::cout << "Hai inserito il numero " << numero << std::endl;
-    return 0;
-}
